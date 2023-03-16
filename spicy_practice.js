9aa909f70cdb2a99cc0845daa6a549bb3147f939
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 13:27:46 -0400

    Create spicy_practice.js

---
+++ spicy_practice.js
@@ -0,0 +1,17 @@
+// Intro to Code with p5.js: Variables and Random
+// Use this file to complete the "Spicy Practice":  Try to import a totally different image. Next, write code in your draw loop that checks to see if the user's mouse comes too close to the image. If it does, move the image to another random part of the screen, essentially "running away" from the user. There are lots of ways to accomplish this, and you may need to use the distance formula or compound conditionals in JavaScript (you can look either one up) to accomplish this.
+
+
+
+function setup(){
+  createCanvas(800, 600);
+  
+  
+  
+}
+
+function draw(){
+  background(220);
+
+  
+}
